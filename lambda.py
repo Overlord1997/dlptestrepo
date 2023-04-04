a6be069fd3b397a33dd0a6b7bf908fa689f7cf27
Tiwari <garvittiwari@kpmg.com>
2023-04-04 21:44:01 +0530

    DLP Master branch 2

--- lambda.py
+++ lambda.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement jenkins DLP code master final
+    # TODO implement jenkins DLP code master final 2
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
