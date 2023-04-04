15dec3fe908438dbea7aab824bbaf3c9e547bc80
Tiwari <garvittiwari@kpmg.com>
2023-04-04 21:41:02 +0530

    Final Push

--- lambda.py
+++ lambda.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement jenkins DLP code master
+    # TODO implement jenkins DLP code master final
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
