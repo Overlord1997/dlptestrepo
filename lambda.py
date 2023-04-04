8ca91d17c6257495222e2eef9ab990accc36d89a
Tiwari <garvittiwari@kpmg.com>
2023-04-04 20:16:49 +0530

    New Code

--- lambda.py
+++ lambda.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement jenkins DLP
+    # TODO implement jenkins DLP code
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
